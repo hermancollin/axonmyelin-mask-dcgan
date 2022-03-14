9026d1e6f7cb91e82ba7fc9c6db85971a5da313d
Armand Collin <armac@videotron.ca>
2022-03-14 16:43:38 -0400

    Added optimizers and training loop

--- main.py
+++ main.py
@@ -160,3 +160,85 @@ if (device.type == 'cuda') and (ngpu > 1):
 
 netD.apply(weights_init)
 print(netD)
+
+criterion = nn.BCELoss()
+
+# Create batch of latent vectors to visualize progression of G
+fixed_z = torch.randn(64, nz, 1, 1, device=device)
+
+# Establish convention for real and fake labels during training
+real_label = 1.
+fake_label = 0.
+
+# Setup Adam optimizers for both G and D
+optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
+
+# Training loop
+
+img_list = []
+G_losses = []
+D_losses = []
+iters = 0
+
+print("Entering training loop")
+for epoch in range(num_epochs):
+    for i, data in enumerate(dataloader, 0):
+
+        # 1 - update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        netD.zero_grad()
+        # all-real batch
+        real_cpu = data[0].to(device)
+        b_size = real_cpu.size(0)
+        label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
+
+        output = netD(real_cpu).view(-1)
+        errD_real = criterion(output, label)
+        errD_real.backward()
+        D_x = output.mean().item()
+
+        # all-fake batch
+        z = torch.randn(b_size, nz, 1, 1, device=device)
+        fake = netG(z)
+        label.fill_(fake_label)
+
+        output = netD(fake.detach()).view(-1)
+        errD_fake = criterion(output, label)
+        errD_fake.backward()
+        D_G_z1 = output.mean().item()
+        err_D = errD_real + errD_fake
+
+        optimizerD.step()
+
+        # 2 - update G network: maximize log(D(G(z)))
+        netG.zero_grad()
+
+        # flip labels for generator cost
+        label.fill_(real_label)
+
+        # additionnal forward pass in D because it was just updated
+        output = netD(fake).view(-1)
+        errG = criterion(output, label)
+        errG.backward()
+        D_G_z2 = output.mean().item()
+
+        optimizerG.step()
+
+        # output training stats
+        if i % 50 == 0:
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                  % (epoch, num_epochs, i, len(dataloader),
+                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+        
+        # save losses
+        G_losses.append(errG.item())
+        D_losses.append(errD.item())
+
+        # Apply G on fixed_z to see progress
+        is_last_iter = (epoch == num_epochs-1) and (i == len(dataloader)-1)
+        if (iters % 500 == 0) or is_last_iter:
+            with torch.no_grad():
+                fake = netG(fixed_z).detach().cpu()
+            img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+
+        iters += 1
