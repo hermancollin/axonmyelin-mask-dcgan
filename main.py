040258c15cc942585af44aa23f6460806345b30b
Armand Collin <armac@videotron.ca>
2022-03-14 15:15:47 -0400

    Added discriminator

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ nz = 100
 # number of feature maps in G
 ngf = 64
 # number of feature maps in D
-ndf = 64
+ndf = 16
 num_epochs = 10
 lr = 0.0002
 beta1 = 0.5
@@ -101,7 +101,11 @@ class Generator(nn.Module):
             nn.Tanh()
             # state size. (nc) x 256 x 256
         )
+    
+    def forward(self, input):
+        return self.main(input)
 
+# Instantiate generator
 netG = Generator(ngpu).to(device)
 
 # Handle multi-gpu if desired
@@ -109,6 +113,50 @@ if (device.type == 'cuda') and (ngpu > 1):
     netG = nn.DataParallel(netG, list(range(ngpu)))
 
 netG.apply(weights_init)
-
-# Print the model
 print(netG)
+
+class Discriminator(nn.Module):
+    def __init__(self, ngpu):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is (nc) x 256 x 256
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 128 x 128
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 64 x 64
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 32 x 32
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 16 x 16
+            nn.Conv2d(ndf * 8, ndf * 16, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 16),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*16) x 8 x 8
+            nn.Conv2d(ndf * 16, ndf * 32, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 32),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*32) x 4 x 4
+            nn.Conv2d(ndf * 32, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+        def forward(self, input):
+            return self.main(input)
+
+# Instantiate discriminator
+netD = Discriminator(ngpu).to(device)
+
+# Handle multi-gpu if desired
+if (device.type == 'cuda') and (ngpu > 1):
+    netD = nn.DataParallel(netD, list(range(ngpu)))
+
+netD.apply(weights_init)
+print(netD)
